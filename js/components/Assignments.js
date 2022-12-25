f1063ac844839cef66ca061f10ed71eda7b4da95
Min Thu Zaw <minthuzaw775@gmail.com>
2022-12-25 21:24:53 +0630

    vue 3 set up

---
+++ js/components/Assignments.js
@@ -0,0 +1,62 @@
+import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
+export default {
+    components: {
+        AssignmentList,
+        AssignmentCreate,
+    },
+    template: `
+    <section class="flex gap-8">
+        <assignment-list :assignments="filter.inProgress" title="In Progress">
+            <assignment-create @add="add"></assignment-create>
+        </assignment-list>
+        
+        <div v-show="showCompleted">
+            <assignment-list 
+                :assignments="filter.completed" 
+                title="Completed" 
+                can-toggle
+                @toggle="showCompleted = !showCompleted"
+            ></assignment-list>
+        </div>
+
+    </section>
+    `,
+
+    data() {
+        return {
+            assignments: [],
+            showCompleted: true
+        };
+    },
+
+    created() {
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments
+            })
+    },
+    computed: {
+        filter() {
+            return {
+                inProgress: this.assignments.filter(
+                    (assignment) => !assignment.complete
+                ),
+                completed: this.assignments.filter((assignment) => assignment.complete),
+            };
+        },
+    },
+
+    methods: {
+        add(name) {
+            this.assignments.push({
+                name: name,
+                completed: false,
+                id: this.assignments.length + 1,
+                tag: 'reading',
+            });
+        },
+    },
+};
